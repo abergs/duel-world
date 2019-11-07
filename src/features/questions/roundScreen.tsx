d51089f7b1a934394cb42fad533d623ac3bf85fc
Anders Åberg <anders.aberg@caspeco.se>
2019-11-08 00:13:04 +0100

    AMAZING end screen

--- src/features/questions/roundScreen.tsx
+++ src/features/questions/roundScreen.tsx
@@ -12,6 +12,9 @@ import {
 } from "features/questions/roundSlice";
 import QuestionScreen from "QuestionScreen";
 import Button from "components/button";
+import Confetti from 'react-confetti';
+import {chime, fart, bubbles} from "features/sound";
+
 
 /**
  *
@@ -20,9 +23,24 @@ import Button from "components/button";
  * Report progress to get new current question
  *
  */
+
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
 export default function RoundScreen(props: any) {
   const round = useSelector(state => state.round);
   const dispatch = useDispatch();
+  const [xy, setxy] = useState([0,0]);
+  const [recycle, setrecycle] = useState(false);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       console.log('This will run after 1 second!')
+//       setrecycle(false);
+//     }, 1000);
+//     return () => clearTimeout(timer);
+//   }, []);
 
   if (round.status == "not-started") {
     return (
@@ -40,12 +58,39 @@ export default function RoundScreen(props: any) {
     );
   }
 
+  const onC = (e:React.MouseEvent) => {
+      console.log(e.clientX, e.clientY);
+      setxy([e.clientX, e.clientY]);
+      setrecycle(true);
+      setTimeout(() => {
+          setrecycle(false)
+      }, 500);
+      
+      if(getRandomInt(10) === 0) {
+          fart.play();
+      } else {
+          bubbles.play();
+      }
+
+  }
+
   if (round.status == "finished") {
     return (
+        <div className="fullscreen" onClick={onC}>
       <Screen>
+           <Confetti
+            recycle={recycle}
+            confettiSource={{
+                x: xy[0],
+                y: xy[1],
+                w: 0,
+                h: 0
+            }}
+           />
         <h1>Your Score is {round.myScore}</h1>
         <Button onClick={e => dispatch(startRound())}>Restart</Button>
       </Screen>
+      </div>
     );
   }
 
--- src/features/sound.ts
+++ src/features/sound.ts
@@ -7,6 +7,9 @@ import bubbleNegativeMp3 from 'assets/bubbleNegative.mp3'
 import errorbuzzMp3 from 'assets/errorbuzz.mp3'
 import chimeMp3 from 'assets/chime.mp3'
 
+import bubblesmp3 from 'assets/bubbles.mp3'
+import fartmp3 from 'assets/fart.mp3'
+
 export const beep1 = new Howl({src: beepMp3});
 export const beep2 = new Howl({src: beep2Mp3});
 export const tickHorror = new Howl({src:tickHorrorMp3});
@@ -14,4 +17,7 @@ export const tickHorror = new Howl({src:tickHorrorMp3});
 export const errorbuzz = new Howl({src:errorbuzzMp3});
 export const popNegative = new Howl({src:bubbleNegativeMp3});
 export const chime = new Howl({src:chimeMp3});
+
+export const bubbles = new Howl({src:bubblesmp3, volume:0.02});
+export const fart = new Howl({src:fartmp3});
 
