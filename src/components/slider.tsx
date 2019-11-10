614e257115514dbc8de8493703429fce2cddc81d
Anders Åberg <anders.aberg@caspeco.se>
2019-11-10 23:12:36 +0100

    works

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -17,15 +17,16 @@ export default function Slider(props: any) {
       console.warn("Done");
   }
 
-  const [{ y, bg, size, progress }, set] = useSpring(() => ({
+  const [{ ready, y, size, progress }, set] = useSpring(() => ({
+    ready: 0,
     y: 0,
-    bg: `linear-gradient(120deg, #f093fb 0%, #f5576c 100%)`,
     size: 1,
     progress: 0,
     config: {
       friction: 10
     },
-    immediate: false //name => down && name === 'y'
+    immediate: false
+    // ready:false //name => down && name === 'y'
   }));
 
   const bind = useDrag(
@@ -59,9 +60,12 @@ export default function Slider(props: any) {
           callback();
       }
 
+      console.warn(actualProgress >= 1 ? 1 : 0);
+
       set({
         y: down ? newY : 0,
-        progress: actualProgress
+        progress: actualProgress,
+        ready: actualProgress >= 1 ? 1 : 0
       });
 
       return newY;
@@ -76,9 +80,12 @@ export default function Slider(props: any) {
     extrapolate: "clamp"
   });
 
+  console.warn("read", progress);
 
   return (
-    <animated.div {...bind()} className="item" style={{ background: bg }}>
+    <animated.div {...bind()} className="item" style={{
+    background: interpolate([progress, ready], (p,r) =>`linear-gradient(${360 * p}deg, ${(r === 0) ? '#f093fb 0%, #f5576c' : '#96fbc4 0%, #f9f586'} 100%)`)
+        }}>
       <animated.div
         className="av"
         style={{
