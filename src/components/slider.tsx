b97d4ce424b90c527365651746f2c449920e18c9
Anders Åberg <anders.aberg@caspeco.se>
2019-11-10 23:32:52 +0100

    Slowed down rotation

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpring, animated, interpolate } from "react-spring";
 import { useGesture, useDrag } from "react-use-gesture";
 import "./styles.css";
@@ -6,20 +6,28 @@ import * as d3scale from "d3-scale";
 
 const easeOutQuad = (t: number): number => 1 + --t * t * t * t * t;
 
+async function sleeper(ms:number) {
+    await new Promise(resolve => setTimeout(resolve, ms))
+  }
+
 export default function Slider(props: any) {
   const targetDistance = 200;
+
+  const [waiting, setWatiting] = useState(false);
 
   const progressCallback = (progress: number) => {
     console.log("Progress", progress);
   };
 
-  const callback = () => {
+  const callback = (): Promise<void> => {
       console.warn("Done");
+      setWatiting(true);
+      return sleeper(1000);
   }
 
   const [{ ready, y, size, progress }, set] = useSpring(() => ({
     ready: 0,
-    y: 0,
+    y: waiting ? 50 : 0,
     size: 1,
     progress: 0,
     config: {
@@ -49,21 +57,36 @@ export default function Slider(props: any) {
       const visualProgress = progressScale(prevPos);
       const adjustment = dragged * (1 - visualProgress);
 
-      const newY = Math.max(0, prevPos + adjustment);
+      let newY = Math.max(0, prevPos + adjustment);
 
       const actualProgress = my / targetDistance;
 
       console.log("p", actualProgress, visualProgress, "length", my);
       progressCallback(actualProgress);
 
+      
+      // WE ARE DONE
       if(!down && actualProgress >= 1) {
-          callback();
+        // let the app know  
+        callback()
+          .then(() => {
+              // after app is ready, reset
+              set({
+                  y:0,
+                  progress: 0,
+                  ready:0
+              })
+          });
+          // set waiting values
+          newY = 50;
+      } else if (!down) {
+          newY = 0;
       }
 
       console.warn(actualProgress >= 1 ? 1 : 0);
 
       set({
-        y: down ? newY : 0,
+        y: newY,
         progress: actualProgress,
         ready: actualProgress >= 1 ? 1 : 0
       });
@@ -84,7 +107,7 @@ export default function Slider(props: any) {
 
   return (
     <animated.div {...bind()} className="item" style={{
-    background: interpolate([progress, ready], (p,r) =>`linear-gradient(${360 * p}deg, ${(r === 0) ? '#f093fb 0%, #f5576c' : '#96fbc4 0%, #f9f586'} 100%)`)
+    background: interpolate([progress, ready], (p,r) =>`linear-gradient(${360 * (p / 4)}deg, ${(r === 0) ? '#f093fb 0%, #f5576c' : '#96fbc4 0%, #f9f586'} 100%)`)
         }}>
       <animated.div
         className="av"
